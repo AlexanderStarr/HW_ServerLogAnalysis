f317cb97de990aafb2f2ec0068aaf29f82082619
Alexander Starr <AlexanderLStarr@gmail.com>
2014-03-06 15:44:58 -0600

    It now calculates the 'lifespan' of each IP address.  Doesn't use UTC offsets.

--- apache.py
+++ apache.py
@@ -2,18 +2,22 @@
 
 from mrjob.job import MRJob
 from mrjob.step import MRStep
-import datetime as dt
+import time
 
 class Apache(MRJob):
 
     def mapper(self, _, line):
-        client = line.split()[0]
-        time_stamp = line.split('[')[1].split(']')[0]
-        unix_time = time_stamp
-        yield (client, time_stamp)
+        elems = line.split()
+        client = elems[0]
+        time_stamp = elems[3][1:]
+        offset = elems[4][:-1]
+        unix_time = time.mktime(time.strptime(time_stamp, '%d/%b/%Y:%H:%M:%S'))
+        yield (client, unix_time)
 
     def reducer(self, client, timevector):
-        yield (client, len(list(timevector)))
+        s_times = sorted(list(timevector))
+        time_delta = s_times[len(s_times)-1] - s_times[0]
+        yield (client, time_delta)
 
 if __name__ == '__main__':
     Apache.run()
