2db1458c01fe8e897ae3c18bbd89697f9d98789e
Alexander Starr <AlexanderLStarr@gmail.com>
2014-03-06 20:31:53 -0600

    Have it now calculating avgs and finding the largest.  It works!

--- apache.py
+++ apache.py
@@ -7,22 +7,52 @@ import time
 class Apache(MRJob):
 
     def mapper1(self, _, line):
+        # First split the line by spaces, not ideal but good enough for our needs.
         elems = line.strip().split()
+        # Get the IP address.
         client = elems[0]
-        time_stamp = elems[3][1:]
+        # Now calculate the UTC offset in seconds.
         offset = elems[4][:-1]
         sign = float(offset[0:1] + '1')
         offset_sec = sign * float(offset[1:3]) * 3600 + sign * float(offset[3:]) * 60
-        unix_time = time.mktime(time.strptime(time_stamp, '%d/%b/%Y:%H:%M:%S')) + offset_sec
-        yield (client, unix_time)
+        # Finally, find the Unix timestamp (with UTC offset) for the log.
+        time_str = elems[3][1:]
+        timestamp = time.mktime(time.strptime(time_str, '%d/%b/%Y:%H:%M:%S')) + offset_sec
+        yield (client, timestamp)
 
     def reducer1(self, client, timevector):
         s_times = sorted(list(timevector))
-        time_delta = s_times[len(s_times)-1] - s_times[0]
-        yield (client, time_delta)
+        stlen = len(s_times)
+        if stlen == 1:
+            return
+        time_total = 0.0
+        for i in range(1, stlen):
+            time_total += s_times[i] - s_times[i-1]
+        avetime = time_total / float(stlen)
+        yield (client, avetime)
+
+    def mapper2(self, client, avetime):
+        yield (client, avetime)
+
+    def reducer2(self, client, avetime):
+        this_avg = list(avetime)[0]
+        try:
+            max_avg = self.MAXAVG
+        except:
+            max_avg = 0.0
+            self.CLIENT = ''
+
+        if this_avg > max_avg:
+            self.MAXAVG = this_avg
+            self.CLIENT = client
+        return
+
+    def reducer_final(self):
+        yield (str(self.CLIENT), str(self.MAXAVG))
 
     def steps(self):
-        return [MRStep(mapper=self.mapper1, reducer=self.reducer1)]
+        return [MRStep(mapper=self.mapper1, reducer=self.reducer1),
+                MRStep(mapper=self.mapper2, reducer=self.reducer2, reducer_final=self.reducer_final)]
 
 if __name__ == '__main__':
     Apache.run()
